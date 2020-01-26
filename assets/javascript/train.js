15b55d3d0a77a33279ea5c9a290ce3a5ac60557b
Jeff <jgunderson819@gmail.com>
2020-01-26 10:47:50 -0600

    added bootstrap, jquery, and firebase language

---
+++ assets/javascript/train.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAp0Xk2QLbD0fGPg0FExNcGioRn_siDynE",
+    authDomain: "service-storage-ec5f5.firebaseapp.com",
+    databaseURL: "https://service-storage-ec5f5.firebaseio.com",
+    projectId: "service-storage-ec5f5",
+    storageBucket: "service-storage-ec5f5.appspot.com",
+    messagingSenderId: "443678778423",
+    appId: "1:443678778423:web:4540674ad09d5760cf4bb2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
